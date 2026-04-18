4901b17e552fccfabd6797691b3f7fb3c6dfe4b7
Luong Duc Anh <ducanh@DucAnh.local>
2026-04-19 01:11:19 +0700

    update

--- src/main/java/AimsProject/src/Cart.java
+++ src/main/java/AimsProject/src/Cart.java
@@ -1,4 +1,4 @@
-package Lab02.AimsProject.src;
+package AimsProject.src;
 
 public class Cart {
     public static final int MAX_NUMBERS_ORDERED = 20;
@@ -12,6 +12,43 @@ public class Cart {
         for(DigitalVideoDisc item : items){
             this.itemsOrdered[qtyOrdered] = item;
             this.qtyOrdered++;
+        }
+    }
+
+    public void print() {
+        System.out.println("***********************CART***********************");
+        System.out.println("Ordered Items:");
+        for (int i = 0; i < qtyOrdered; i++) {
+            System.out.println((i + 1) + ". " + itemsOrdered[i].toString());
+        }
+        System.out.println("Total cost: " + totalCost() + " $");
+        System.out.println("***************************************************");
+    }
+
+    public void searchById(int id) {
+        boolean found = false;
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].getId() == id) {
+                System.out.println("Match found by ID: " + itemsOrdered[i].toString());
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("No match found for ID: " + id);
+        }
+    }
+
+    public void searchByTitle(String title) {
+        boolean found = false;
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].isMatch(title)) {
+                System.out.println("Match found by Title: " + itemsOrdered[i].toString());
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("No match found for Title: " + title);
         }
     }
 
@@ -35,6 +72,7 @@ public class Cart {
         boolean found = false;
         for(int i =0; i< itemsOrdered.length; i++){
             if(itemsOrdered[i].equals(disc)){
+                found = true;
                 for(int j = i; j < qtyOrdered - 1; j++){
                     itemsOrdered[j] = itemsOrdered[j+1];
                 }
